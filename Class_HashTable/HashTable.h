c1089bf88f7aba003d21e0cfa6faad90f635865f
Alexandr Smolaykov <sphinx414@gmail.com>
2015-11-18 13:18:22 +0300

    Major changes in project structure

---
+++ Class_HashTable/HashTable.h
@@ -0,0 +1,30 @@
+#ifndef HASHTABLE_H    // если имя CPPSTUDIO_H ещё не определено
+#define HASHTABLE_H
+
+template <size_t S, typename K, typename T>
+class HashTable
+{
+public:
+
+	HashTable();
+	HashTable(const HashTable&);
+	~HashTable();
+	HashTable& operator = (const HashTable&);
+
+	bool Insert(K, T);
+	int Search(K);
+	void Remove(int);
+
+private:
+	// methods
+	int hashFunction(K);
+	int linearInvestigation(size_t, size_t);
+
+	// fields
+	enum cellStatus { Empty, Busy, Delete };
+	struct cell { cellStatus status; K key; T data; };
+	cell* hashTable_;
+};
+
+#include "HashTable_Implementation.h"
+#endif
